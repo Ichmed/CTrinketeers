80a0de85e31b136552df2c2c4a783f08e0b7231b
Ichmed <mijomeyer@t-online.de>
2017-04-17 20:27:42 +0200

    Update Chunk.cpp

--- Chunk.cpp
+++ Chunk.cpp
@@ -1,6 +1,33 @@
+#include <iostream>
+
 class Chunk
 {
+	int lastAge;
+	
 	void load(String s)
-  {
-  }
+	{
+		//Check if Chunk exists on disk
+		//If yes load the chunk and apply aging
+		//Else Create New Chunk
+	}
+	
+	void age(int currentAge)
+	{
+		//Calcuate age distance
+		//Change Geography
+		//Age exisitng entities
+		//Create new entities
+		//Set lastAge = currentAge
+	}
+	
+	void unload(String s)
+	{
+		//Save Chunk to disk
+		//Call destructor
+	}
+	
+	String createFileName(int x, int y, int z)
+	{
+		//Creates the name of a FILE containing the chunk data at X/Y/Z
+	}
 }
