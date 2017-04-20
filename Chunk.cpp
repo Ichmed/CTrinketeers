23071fe41150ebb88c92b08ccadadd082b6aac96
Ichmed <mijomeyer@t-online.de>
2017-04-20 17:39:36 +0200

    Update Chunk.cpp

--- Chunk.cpp
+++ Chunk.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
 
+class ChunkManager
+{
+	int range;
+	
+	Chunk[][][] chunks;
+	
+	void shiftGrid(int x, int y, int z)
+	{		
+	}
+	
+	Chunk loadChunk(int x, int y, int z)
+	{
+		Chunk c = new Chunk();
+		c.load(x, y, z);
+	}
+	
+	void unloadChunk(Chunk c, int x, int y, int z)
+	{
+		c.save(x, y, z);
+		delete c;
+	}
+}
+
 class Chunk
 {
 	int lastAge;
 	
-	void load(String s)
+	void load(int x, int y, int z)
 	{
 		//Check if Chunk exists on disk
+		FILE * data = fopen(createFileName(x, y, z), "r+");
 		//If yes load the chunk and apply aging
+		if(data)
+		{
+			//load data
+		}			
 		//Else Create New Chunk
+		fclose(data);
 	}
 	
 	void age(int currentAge)
@@ -19,15 +48,18 @@ class Chunk
 		//Create new entities
 		//Set lastAge = currentAge
 	}
-	
-	void unload(String s)
+		
+	void save(int x, int y, int z)
 	{
-		//Save Chunk to disk
-		//Call destructor
 	}
 	
 	String createFileName(int x, int y, int z)
 	{
 		//Creates the name of a FILE containing the chunk data at X/Y/Z
+		String path = "./data/" += Game.getCurrentGame().getName() += "/";
+		path += string::to_string(x) += "x";		
+		path += string::to_string(y) += "x";
+		path += string::to_string(z) += ".ctc";
+		return path;
 	}
 }
